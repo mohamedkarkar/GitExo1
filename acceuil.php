d66c5c1ca36a7546fe7ba713c11c18c3ae87b15d
Mohamed-Lamine KARKAR <mkark@lmdsio.com>
2025-09-17 14:59:36 +0200

    version 1

--- acceuil.php
+++ acceuil.php
@@ -1,6 +1,16 @@
 <?php
-$Prenom=$_POST['prenom'];
-$Nom=$_POST['nom'];
 
-echo "Bonjour $Prenom $Nom";
+$login = isset($_POST['login']) ? $_POST['login'] : '';
+$password = isset($_POST['password']) ? $_POST['password'] : '';
+
+
+$host = $_SERVER['HTTP_HOST'];
+$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+
+
+if ($login === 'admin' && $password === 'azerty') {
+
+    header("Location: http://$host$uri/profil.html");
+    exit();
+}
 ?>
